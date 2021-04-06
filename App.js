6b2742194c8b66b972a160e23cd91c998f062b16
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-06 13:55:38 +0900

    Axios 에러나서 실패
    openweathermap 사이트에서 API_KEY를 받아서 current weather data를 가져옴. (const API_KEY)
    
    Axios 설치
    
    ======================
    Axios 에러나서 node_modules, package.lock 지우고 재설치 하래서 하다가 재설치 안돼서 폭망
    node_modules덩어리가 커서 쓰레기통에도 안넣고 영구삭제해서 망함 ..

--- App.js
+++ App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { Alert } from 'react-native';
 import Loading from "./Loading";
 import * as Location from 'expo-location';
+import axios from "axios";
+
+const API_KEY = "99919bb4c713e197195d5c6ace9d21df";
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  }
   getLocation = async() => {
     try {
       await Location.requestPermissionsAsync();
-      const { coords: { latitude, longitude }} = await Location.getCurrentPositionAsync();
-      // Send to API and get weather!
+      const { 
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("사용자의 위치를 찾을 수 없습니다", "유감입니다 😥");
